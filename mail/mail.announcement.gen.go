d3a5a228b09642d8e7e01a66d182f41226d2860c
jiharal <jihar.akakom14@gmail.com>
2019-02-27 23:07:17 +0800

    Add receipt page

--- mail/mail.announcement.gen.go
+++ mail/mail.announcement.gen.go
@@ -7,13 +7,29 @@ import (
 	"github.com/matcornic/hermes"
 )
 
-type EmailContentAnnouncement struct {
-	MimeType     MimeType
-	Organization string
-	Intros       []string
-	Greeting     string
-	Signature    string
-}
+type (
+	EmailContentAnnouncement struct {
+		MimeType     MimeType
+		Organization string
+		Intros       []string
+		Greeting     string
+		Signature    string
+	}
+
+	Receipt struct {
+		MimeType     MimeType
+		URL          string
+		CustomerName string
+		Organization string
+		Intros       []string
+		Greeting     string
+		Signature    string
+		TableData    [][]hermes.Entry
+		ActionIntro  string
+		ButtonText   string
+		ButtonLink   string
+	}
+)
 
 func GenerateAnnouncementEmail(option EmailContentAnnouncement) (string, error) {
 	h := hermes.Hermes{
@@ -38,5 +54,50 @@ func GenerateAnnouncementEmail(option EmailContentAnnouncement) (string, error) 
 		return h.GenerateHTML(email)
 	}
 
+	return h.GeneratePlainText(email)
+}
+
+func GenerateReceipt(option Receipt) (string, error) {
+	h := hermes.Hermes{
+		Product: hermes.Product{
+			Name: option.Organization,
+			Link: option.URL,
+			Copyright: fmt.Sprintf("Copyright © %d %s. All rights reserved.",
+				time.Now().Year(), option.Organization),
+		},
+	}
+
+	email := hermes.Email{
+		Body: hermes.Body{
+			Name:   option.CustomerName,
+			Intros: option.Intros,
+			Table: hermes.Table{
+				Data: option.TableData,
+				Columns: hermes.Columns{
+					CustomWidth: map[string]string{
+						"Item":  "20%",
+						"Price": "15%",
+					},
+					CustomAlignment: map[string]string{
+						"Price": "right",
+					},
+				},
+			},
+			Actions: []hermes.Action{
+				{
+					Instructions: option.ActionIntro,
+					Button: hermes.Button{
+						Text: option.ButtonText,
+						Link: option.ButtonLink,
+					},
+				},
+			},
+		},
+	}
+
+	switch option.MimeType {
+	case MimeTypeHtml:
+		return h.GenerateHTML(email)
+	}
 	return h.GeneratePlainText(email)
 }
